73938d339616d29dca263411971159078456bd22
KacperJanczyk220646 <112700204+KacperJanczyk220646@users.noreply.github.com>
2023-06-20 22:44:21 +0200

    streamlit final deliverable pages

---
+++ Streamlit/Introduction.py
@@ -0,0 +1,140 @@
+"""
+Introduction Page for GreenScape Project in Streamlit
+
+This script creates a Streamlit web page for the GreenScape project, which aims to improve the Green score in Breda. The page contains 5 multiple tabs, each displaying different information related to the project.
+
+Usage:
+    - Run the script to launch the introduction page in Streamlit.
+
+Dependencies:
+    - streamlit
+    - PIL (Python Imaging Library)
+
+"""
+
+import streamlit as st
+from PIL import Image
+
+
+def main():
+    """
+    Main function to create the Streamlit page for the GreenScape project.
+    """
+    st.title("Project for improving Green score in Breda (GreenScape)")
+
+    # Display image
+    image1 = Image.open("C:/Users/kacpe/Documents/GreenScape-1/Streamlit/Data/park.png")
+    st.image(image1)
+
+    # Create tabs
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Business Understanding", "AI Canvas", "Roadmap", "Team members", "Conclusion"])
+
+    with tab1:
+        st.header("Business Understanding")
+        st.write("By analyzing the factors that have an impact on the Green Index score, the client can allocate resources efficiently and effectively to address the most pressing environmental challenges. By tracking changes in each factor over time, Breda can set realistic goals and monitor its progress toward becoming more sustainable in a completely data-driven manner. We will analyze the correlation between Quality of life, Public Safety, Segregation, and Environmental statistics with the green index score to determine if we are able to predict a green index score using regression task for each neighborhood in Breda.")
+        st.subheader("Research questions:")
+        st.write("1. What are the factors that contribute to the green index score?")
+        st.write("2. Based on these factors, can we predict a green index score?")
+
+    with tab2:
+        st.header("AI Canvas")
+        image2 = Image.open(r"C:\Users\kacpe\Documents\GreenScape-1\Streamlit\Data\AI_Canvas.png")
+        st.image(image2)
+
+    with tab3:
+        st.header("Project's Roadmap")
+        image3 = Image.open(r"C:\Users\kacpe\Documents\GreenScape-1\Streamlit\Data\Roadmap_Image.png")
+        st.image(image3)
+        st.subheader("Week 4: Business & Data Understanding")
+        business_steps = [
+            "Familiarize ourselves with the project goals and objectives. (including working with Trello workspace)",
+            "Conduct meetings with the client to gain a deeper understanding of their requirements.",
+            "Identify the key factors affecting the Green Index score (Quality of life, Public Safety, Segregation, Environmental statistics).",
+            "Gather relevant data sources for each factor and understand their structure and format.",
+            "Conduct exploratory data analysis (EDA) to gain insights into the data and identify any data quality issues or missing values."
+        ]
+        st.markdown(f"1. {business_steps[0]}")
+        st.markdown(f"2. {business_steps[1]}")
+        st.markdown(f"3. {business_steps[2]}")
+        st.markdown(f"4. {business_steps[3]}")
+        st.markdown(f"5. {business_steps[4]}")
+
+        st.subheader("Week 5: Data Preparation")
+        data_steps = [
+            "Cleanse and preprocess the data to address any data quality issues, such as missing values, outliers, or inconsistencies.",
+            "Perform feature engineering to create new variables or transformations that may improve the predictive power of the model.",
+            "Integrate the data from different sources and ensure it is in a suitable format for modeling.",
+            "Split the dataset into training, validation, and testing sets to evaluate the performance of the model accurately.",
+            "Document the data preparation steps taken for transparency and reproducibility."
+        ]
+        st.markdown(f"1. {data_steps[0]}")
+        st.markdown(f"2. {data_steps[1]}")
+        st.markdown(f"3. {data_steps[2]}")
+        st.markdown(f"4. {data_steps[3]}")
+        st.markdown(f"5. {data_steps[4]}")
+
+        st.subheader("Week 6: Modeling")
+        modeling_steps = [
+            "Select an appropriate regression algorithm to predict the Green Index score based on the identified factors.",
+            "Train the regression model on the prepared dataset.",
+            "Optimize the model by tuning hyperparameters and using techniques such as cross-validation.",
+            "Evaluate the model's performance using appropriate evaluation metrics (e.g., mean squared error, R-squared).",
+            "Consider ensemble methods or feature selection techniques to improve model performance if necessary."
+        ]
+        st.markdown(f"1. {modeling_steps[0]}")
+        st.markdown(f"2. {modeling_steps[1]}")
+        st.markdown(f"3. {modeling_steps[2]}")
+        st.markdown(f"4. {modeling_steps[3]}")
+        st.markdown(f"5. {modeling_steps[4]}")
+
+        st.subheader("Week 7: Evaluation")
+        evaluation_steps = [
+            "Assess the model's predictive accuracy by comparing the predicted Green Index scores with the actual scores in the validation set.",
+            "Perform additional analysis to understand the model's strengths and limitations.",
+            "Identify any biases or limitations in the data and model, and discuss potential ways to mitigate them.",
+            "Collaborate with the client to validate the model's usefulness in addressing their goals and requirements.",
+            "Document the evaluation results and recommendations for future improvements."
+        ]
+        st.markdown(f"1. {evaluation_steps[0]}")
+        st.markdown(f"2. {evaluation_steps[1]}")
+        st.markdown(f"3. {evaluation_steps[2]}")
+        st.markdown(f"4. {evaluation_steps[3]}")
+        st.markdown(f"5. {evaluation_steps[4]}")
+
+        # Week 8: Deployment
+        st.subheader("Week 8: Deployment")
+        deployment_steps = [
+            "Finalize the regression model based on the evaluation and feedback received.",
+            "Apply the trained model to predict the Green Index scores for each neighborhood in Breda.",
+            "Generate a comprehensive report summarizing the findings, including the model's performance, feature importance, and insights into the factors affecting the Green Index score.",
+            "Present the report to the client and stakeholders, explaining the results and potential implications for resource allocation and sustainability efforts.",
+            "Provide recommendations on how to monitor and update the model in the future as new data becomes available.",
+            "Collaborate with the client to develop an implementation plan for integrating the model into their decision-making processes."
+        ]
+        st.markdown(f"1. {deployment_steps[0]}")
+        st.markdown(f"2. {deployment_steps[1]}")
+        st.markdown(f"3. {deployment_steps[2]}")
+        st.markdown(f"4. {deployment_steps[3]}")
+        st.markdown(f"5. {deployment_steps[4]}")
+        st.markdown(f"6. {deployment_steps[5]}")
+
+
+    with tab4:
+        st.header("Our team")
+        image4 = Image.open(r"C:\Users\kacpe\Documents\GreenScape-1\Streamlit\Data\Our_team.png")
+        st.image(image4, width=800)
+
+    with tab5:
+        st.header("Conclusion")
+        st.divider()
+        st.write('The model is able to predict a change in green score according to a change of value in one of the selected criteria. However, we would like to highlight the central pain-point with any project of this nature.')
+        st.divider()
+        st.subheader('Correlation does not equal Causation.')
+        st.divider()
+        st.write('Even though the algorithm predicts a change in value if average income value increases, it does not mean that the change in green score is a direct consequence of a higher average salary.')
+        st.write('We realize this is due to the fact that the green score is calculated based on the total amount of green area visible in a geographic region.')
+        st.write('Despite this, we believe that the factors presented in the algorithm go a long way in enabling more green areas being added to certain neighborhoods.')
+
+
+if __name__ == "__main__":
+    main()
---
+++ Streamlit/Pages/2_EDA.py
@@ -0,0 +1,224 @@
+import plotly.graph_objs as go
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+st.set_page_config(page_title='Exploritory Data Analysis', layout="wide")
+st.title('Exploritory Data Analysis')
+
+streamlit_path = 'C:/Users/kacpe/Documents/GreenScape-1/EDA/Simona/scripts/dashboard.py'
+file_path = 'C:/Users/kacpe/Documents/GreenScape-1/EDA/Simona/scripts/Preprocessed_Dataset.csv'
+
+
+def read_file_from_relative_path(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content.index
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+        return None
+    except IOError:
+        print(f"Error reading file '{file_path}'.")
+        return None
+
+
+file_content = read_file_from_relative_path(file_path)
+if file_content:
+    df_read = pd.read_csv(file_path, index_col=0)
+    print('Sucessfully read the .csv!')
+
+
+def preprocess(df):
+    df['Year'] = df['Year'].astype(str)
+    df['date'] = pd.to_datetime(df['date'])
+    df['months'] = df['date'].dt.month_name(locale='English')
+    df = df.sort_values(by='date', ascending=True)
+    df = df.groupby(by=['date', 'Year', 'months',
+                    'Neighborhood', 'regions']).mean()
+    df.reset_index(inplace=True)
+    return df
+
+
+def scatterplot(df, x, y, title):
+    fig = px.scatter(df,
+                     x=x,
+                     y=y,
+                     animation_frame="Year",
+                     size="Population",
+                     animation_group="Neighborhood",
+                     hover_name="Neighborhood",
+                     log_x=True,
+                     size_max=60,
+                     color="regions",
+                     title=title)
+    fig["layout"].pop("updatemenus")
+    return fig
+
+
+def barplot(df, y):
+    fig = px.bar(df, x='date', y=y, color='months',
+                 title='Barchart on Monthly level')
+    fig1 = px.bar(df, x='Year', y=y, color='Year',
+                  title='Barchart on Yearly level')
+
+    fig.update_xaxes(minor=dict(ticks="inside", showgrid=True))
+    fig1.update_xaxes(minor=dict(ticks="inside", showgrid=True))
+
+    return fig, fig1
+
+
+def histogram(df, y):
+    fig = px.histogram(df, x="date", y=y, histfunc="avg",
+                       nbins=30, text_auto='.2f', title='Histogram on Mothly level')
+    fig.add_hline(y=df[y].mean(), line_dash="dot",
+                  annotation_text="Average green score of all time",
+                  annotation_position="bottom right")
+    fig.update_xaxes(minor=dict(ticks="inside", showgrid=True))
+
+    return fig
+
+
+def headmap(df):
+    df_groupby = df.groupby(by='regions')[
+        ['green_score', 'income', 'livability_score_x', 'Registered nuisance (number)', 'Population']].mean().reset_index()
+    fig = px.imshow(df_groupby.corr(), text_auto=True)
+    return fig
+
+
+def linechart(df, y):
+    fig = px.line(df, x="Year", y=y)
+    fig.update_traces(textposition="bottom right")
+
+    return fig
+
+
+def pie_charts(df, regions):
+    fig = make_subplots(rows=2, cols=2, specs=[[{'type': 'domain'}, {'type': 'domain'}],
+                                               [{'type': 'domain'}, {'type': 'domain'}]])
+    fig.add_trace(go.Pie(labels=df_sorted['regions'], values=df_sorted['green_score'], name="Green Score"),
+                  1, 1)
+    fig.add_trace(go.Pie(labels=df_sorted['regions'], values=df_sorted['income'], name="Income"),
+                  1, 2)
+    fig.add_trace(go.Pie(labels=df_sorted['regions'], values=df_sorted['livability_score_x'], name="Livability"),
+                  2, 1)
+    fig.add_trace(go.Pie(labels=df_sorted['regions'], values=df_sorted['Registered nuisance (number)'], name="Public Nuisance"),
+                  2, 2)
+    fig.update_traces(hole=.4, hoverinfo="label+percent+name")
+    fig.update_layout(template='plotly_dark')
+    return fig
+
+
+df_sorted = preprocess(df_read)
+
+
+scatterplot_livability = scatterplot(df_sorted, 'livability_score_x', 'green_score',
+                                     'Correlation between Livability Score and Green Score Index')
+scatterplot_income = scatterplot(df_sorted, 'income', 'green_score',
+                                 'Correlation between Income score and Green Score Index')
+scatterplot_public_nuisance = scatterplot(df_sorted, 'Registered nuisance (number)', 'green_score',
+                                          'Correlation between Public Nuisance and Green Score Index')
+barplot_months, barplot_years = barplot(df_sorted, 'green_score')
+histogram_months = histogram(df_sorted, 'green_score')
+headmap_plot = headmap(df_sorted)
+pie_charts_plot = pie_charts(df_sorted, 'regions')
+
+df_groupby_years = df_sorted.groupby(by="Year").agg(avg_green_score=("green_score", 'mean'),
+                                                    avg_livability_score_x=(
+                                                        'livability_score_x', 'mean'),
+                                                    avg_total_houses=(
+                                                        'TotalHouses', 'mean'),
+                                                    avg_population=(
+                                                        'Population', 'mean'),
+                                                    avg_income=('income', 'mean')).round(2)
+df_groupby_months = df_sorted.groupby(by="months").agg(avg_green_score=("green_score", 'mean'),
+                                                       avg_livability_score_x=(
+                                                           'livability_score_x', 'mean'),
+                                                       avg_total_houses=(
+                                                           'TotalHouses', 'mean'),
+                                                       avg_population=(
+                                                           'Population', 'mean'),
+                                                       avg_income=('income', 'mean')).round(2)
+df_groupby_neighborhoods = df_sorted.groupby(by="Neighborhood").agg(avg_green_score=("green_score", 'mean'),
+                                                                    avg_livability_score_x=(
+                                                                        'livability_score_x', 'mean'),
+                                                                    avg_total_houses=(
+                                                                        'TotalHouses', 'mean'),
+                                                                    avg_population=(
+                                                                        'Population', 'mean'),
+                                                                    avg_income=('income', 'mean')).round(2)
+df_groupby_regions = df_sorted.groupby(by="regions").agg(avg_green_score=("green_score", 'mean'),
+                                                         avg_livability_score_x=(
+                                                             'livability_score_x', 'mean'),
+                                                         avg_total_houses=(
+                                                             'TotalHouses', 'mean'),
+                                                         avg_population=(
+                                                             'Population', 'mean'),
+                                                         avg_income=('income', 'mean')).round(2)
+
+
+def reset_index(df):
+    df_reset_index = df.reset_index()
+    return df_reset_index
+
+
+df_reset_index = reset_index(df_groupby_years)
+linechart_years = linechart(df_reset_index, 'avg_green_score')
+
+
+dropdown_options = ['Months', 'Years']
+selected_option = st.selectbox(
+    'Select an option:', dropdown_options, format_func=lambda x: x)
+
+col1, col2 = st.columns(2)
+if selected_option == 'Months':
+    st.markdown('**_The green score index is displayed on both charts on a monthly basis. The highest average green score for all months was recorded between September and October 2017 - 39.94 and the lowest was recorded in September 2015 - 16.72. As a result, the average green score for all the months was approximately 27._**')
+    with col1:
+        st.plotly_chart(barplot_months, theme='streamlit')
+    with col2:
+        st.plotly_chart(histogram_months, theme='streamlit')
+elif selected_option == 'Years':
+    st.markdown('**_The green score index is displayed on both charts on a yearly basis. The highest average green score for all years was recorded on 2015 - 28.12 and the lowest was recorded on 2018 - 23.5._**')
+    with col1:
+        st.plotly_chart(barplot_years, theme='streamlit',
+                        use_container_width=True)
+    with col2:
+        st.plotly_chart(linechart_years, theme='streamlit',
+                        use_container_width=True)
+
+
+tab3, tab4, tab5 = st.tabs(['Livability', 'Income', 'Public Nuisance'])
+col3, col4 = st.columns(2)
+
+with tab3:
+    st.markdown('**_The scatter plot depicts the relationships between the livability score and the green score for all regions in Breda for the period of 2014 to 2018 according to the size of the dots indicating the population for all regions in Breda. As can be seen in the graph, the green score is indicated on the Y axis, while the livability is represented on the X axis._**')
+    st.plotly_chart(scatterplot_livability,
+                    theme="streamlit", use_container_width=True)
+with tab4:
+    st.markdown('**_The scatter plot depicts the relationships between the income and the green score for all regions in Breda for the period of 2014 to 2018 according to the size of the dots indicating the population for all regions in Breda. As can be seen in the graph, the green score is indicated on the Y axis, while the income is represented on the X axis._**')
+    st.plotly_chart(scatterplot_income,
+                    theme="streamlit", use_container_width=True)
+with tab5:
+    st.markdown('**_The scatter plot depicts the relationships between the registered public nuisance and the green score for all regions in Breda for the period of 2014 to 2018 according to the size of the dots indicating the population for all regions in Breda. As can be seen in the graph, the green score is indicated on the Y axis, while the registered public nuisance is represented on the X axis._**')
+    st.plotly_chart(scatterplot_public_nuisance,
+                    theme="streamlit", use_container_width=True)
+
+with col3:
+    st.subheader('Heatmap')
+    st.plotly_chart(headmap_plot, theme="streamlit", use_container_width=True)
+    st.markdown('**_The heatmap is a graphical representation of data using colors to indicate the level of activity. Darker colors are used to indicate low activity and brighter colors to indicate high activity. Between green score and livability score there is moderate correlation as green score increases, the livability score decreases._**')
+with col4:
+    dropdown_options1 = ['Years', 'Months', 'Regions', 'Neighborhoods']
+    tables = {'Years': df_groupby_years['avg_green_score'],
+              'Months': df_groupby_months['avg_green_score'],
+              'Regions': df_groupby_regions['avg_green_score'],
+              'Neighborhoods': df_groupby_neighborhoods['avg_green_score']}
+    selected_option = st.selectbox(
+        'Select an option:', dropdown_options1, format_func=lambda x: x)
+    if selected_option in tables.keys():
+        st.write("Table for", selected_option)
+        st.dataframe(tables[selected_option], use_container_width=True)
+    else:
+        st.write("No table selected.")
